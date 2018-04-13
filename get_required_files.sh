0a40ccdfac5871a2fb79beb6f5f45d1cacd308c6
Ryan Pham <phamcake@gmail.com>
2018-04-12 22:43:27 -0700

    setup: remove parser from required files

--- get_required_files.sh
+++ get_required_files.sh
@@ -4,8 +4,3 @@ echo 'downloading and unzipping data'
 wget ${site}'data.tar'
 tar -xvf data.tar
 rm data.tar
-
-echo 'downloading and unzipping parser'
-wget ${site}'parser.tar'
-tar -xvf parser.tar
-rm parser.tar
