e570d3d58bb08c62f54d2522b9a70161f707d446
Ryan Pham <phamcake@gmail.com>
2018-05-14 23:03:41 -0700

    metrics: added more detailed debugging to predicate subset calculator

--- calculate_predicate_f1.py
+++ calculate_predicate_f1.py
@@ -16,6 +16,10 @@ def main():
                         help='Source graphs.')
     parser.add_argument('predicted', type=str,
                         help='Predicted graphs.')
+    parser.add_argument('--top', type=int, default=10,
+            help='Prints top n predicates with debugging')
+    parser.add_argument('--debug', action='store_true', default=False,
+            help='Turns on debugging output.')
     args = parser.parse_args()
 
 
@@ -26,9 +30,26 @@ def main():
     gold_counts = get_counter()
     pred_counts = get_counter(correct=True)
 
-    for label, prediction in tqdm(zip(labels, predictions), total=len(labels)):
+    errors = {
+        'surface': set(),
+        'abstract': set(),
+        'overpredicted': Counter(),
+        'underpredicted': Counter()
+    }
+    sig_diff = []
+
+    for i, (label, prediction) in tqdm(enumerate(zip(labels, predictions)), total=len(labels)):
         gold_surface, gold_abstract = get_predicates(label)
         pred_surface, pred_abstract = get_predicates(prediction)
+
+        if args.debug:
+            errors['surface'].update(gold_surface)
+            errors['surface'].update(pred_surface)
+            errors['abstract'].update(gold_abstract)
+            errors['abstract'].update(pred_abstract)
+
+            if len(label.split()) > len(prediction.split()) + 15:
+                sig_diff.append((i, label, prediction))
 
         gold_counts['abstract'] += len(gold_abstract)
         gold_counts['surface'] += len(gold_surface)
@@ -36,9 +57,11 @@ def main():
         pred_counts['surface']['total'] += len(pred_surface)
 
         pred_counts['surface']['correct'] += count_correct(gold_surface,
-                                                          pred_surface)
+                                                           pred_surface,
+                                                           errors)
         pred_counts['abstract']['correct'] += count_correct(gold_abstract,
-                                                           pred_abstract)
+                                                            pred_abstract,
+                                                            errors)
 
     print('Abstract results')
     print('------------------------------')
@@ -71,8 +94,25 @@ def main():
     print('Recall: {:.2f}'.format(recall))
     print('F1: {:.2f}\n'.format(2 * (precision * recall) / (precision + recall)))
 
+    if args.debug:
+        print('Number of Abstract predicates: {}'.format(len(errors['abstract'])))
+        print('Number of Surface predicates: {}'.format(len(errors['surface'])))
+        print('Number of pairs with large length difference: {}'.format(len(sig_diff)))
 
-def count_correct(gold, pred):
+        for key in ['overpredicted', 'underpredicted']:
+            errors[key] = Counter({ pred : errors[key][pred]
+                                    for pred in errors[key]
+                                    if errors[key][pred] > 20 })
+            print()
+            print('Most commonly {} words:'.format(key))
+            print(errors[key].most_common(args.top))
+
+
+        print('importing ipdb for debugging purposes')
+        import ipdb; ipdb.set_trace()
+
+
+def count_correct(gold, pred, errors):
     correct = 0
     counts = Counter()
     counts.update(gold)
@@ -80,6 +120,11 @@ def count_correct(gold, pred):
         if counts[val]:
             correct += 1
             counts[val] -= 1
+        else:
+            errors['overpredicted'][val] += 1
+    for val in counts.keys():
+        if counts[val] > 0:
+            errors['underpredicted'][val] += 1
     return correct
 
 
