c77fca16356cd9b279af73482da6e05f9864d36a
Ryan Pham <phamcake@gmail.com>
2018-06-12 22:12:27 -0700

    setup: update get_required_files.sh to include pretrained models

--- get_required_files.sh
+++ get_required_files.sh
@@ -1,7 +1,14 @@
 site='https://homes.cs.washington.edu/~ryanp97/'
 data='datasets.tar'
+models='models.tar'
 
 echo 'downloading and unzipping data'
 wget $site.$data
 tar -xvf $data
 rm $data
+
+
+echo 'downloading and unzipping pretrained models'
+wget $site.$models
+tar -xvf $models
+rm $models
